5d401adf18418554356a6e7ac1a17b5e56aaae6f
ChaohuiWang3 <wch3145863356@gmail.com>
2024-05-13 14:25:37 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,280 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useState, useEffect, useMemo} from 'react';
+import { ForceGraph2D } from 'react-force-graph';
+import graph from './graph.json';   //build matrix
+import graph_data from './graph_data.json';  //build graph
+
 
 function App() {
+  const linkLabel = link =>{ //show the information about the link
+    return `link: ${link.source.id} -> ${link.target.id}, ${link.sign}`;
+  }
+
+
+  const [matrixData, setMatrixData] = useState([]);   //use graph to build the matrix
+
+
+  useEffect(() => {
+    const nodes = Object.keys(graph);  //extract all nodes in graph
+    const initialMatrix = nodes.map(row => (
+      nodes.map(col => {   //use 'map' to iterate, build a size[nodes] × size[nodes] matrix
+        const found = graph[row]?.find(link => link[0] === col); //for graph[row], find whether there is a link(relation) with each column for example, three nodes, for graph[a], check link with each node(a,b,c). If there is a link, a has relation with that vertex, the found is recorded
+        return found ? found[1] : '/'; // if there is not a link, a sign '/' is recorded 
+      })
+    ));
+    setMatrixData(initialMatrix);  //build the matrix
+  }, []);
+
+
+  const handleInputChange = (event, row, col) => { //use keyboard to change the relation between any two vertices
+    const value = event.target.value;
+    const currentValue = matrixData[row][col];
+    if (currentValue === '/') { //if '/' is chosen to be changed
+      alert("'/' value can not be changed"); //alert the user
+      return; 
+    }
+    const updatedMatrix = [...matrixData];
+    updatedMatrix[row][col] = value; //update the matrix, change the selected link into new value 
+    setMatrixData(updatedMatrix);
+  };
+
+
+  const handleSubmit = async () => { //submit the changes to balance.py
+    for (let row of matrixData) { //check if there are any invalid characters
+      for (let cell of row) {
+        if (cell !== '+' && cell !== '-' && cell !== '/') { //only +,-,/ are allowed to exist in the matrix
+          alert("Invalid entry detected. Only '+', '-', and '/' are allowed.");
+          return; 
+        }
+      }
+    }
+
+    const updatedGraph = {}; //transfer new matrix into graph format
+    Object.keys(graph).forEach((node, i) => {
+      updatedGraph[node] = []; //each node of updatedGraph has its own empty array
+      matrixData[i].forEach((value, j) => {
+        const targetNode = Object.keys(graph)[j]; //check the relation of this node with all nodes in the matrix
+        if (value !== '/') { // '/' relation is ignored
+          updatedGraph[node].push([targetNode, value]); //add the link value into the node's array
+        }
+      });
+    });
+
+    const response = await fetch('http://127.0.0.1:5000/api/update-graph', {
+      method: 'POST',
+      headers: { //send a request to backend program balance.py
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ graphData: updatedGraph }) //send updatedGraph as a json file
+    });
+
+    if (response.ok) { //check the reponse
+      console.log('Update successful');
+      //console.get(graph_data)
+    } else {
+      console.error('Failed to update graph data');
+    }
+  };
+
+
+  //color rules for balanced graphs
+  const BalancedNodeColor = useMemo(() => { //depending on the isbalanced result to draw the color of nodes
+    if (!graph_data.isbalanced) { //if the graph is not balanced
+      return () => 'grey'; // All nodes are grey 
+    }
+    
+    //if the graph is balanced
+    const nodeColorMap = new Map(); //a Map to set the color of nodes
+    const firstNodeId = graph_data.originalgraphdata.nodes[0].id;
+    nodeColorMap.set(firstNodeId, 'red'); // First node is red color
+  
+    graph_data.originalgraphdata.links.forEach(link => {
+      const sourceColor = nodeColorMap.get(link.source);
+      const targetColor = link.sign === '+' ? sourceColor : (sourceColor === 'red' ? 'blue' : 'red'); //if the node is positive relation with the first node, it is red color; if it is negative relation, the node is blue color 
+      nodeColorMap.set(link.target, targetColor);
+    });
+    return node => nodeColorMap.get(node.id); //draw the node color
+  }, []);
+
+  //render the nodes of balanced graph
+  const RenderBalancedNode = (node, ctx, globalScale) => { 
+    const label = `node: ${node.id}`; //the label is the name of the node(eg. A, B...)
+    const fontSize = 12 / globalScale; 
+    const color = BalancedNodeColor(node); //the color of the node is defined by BalancedNodeColor
+
+    // draw the node
+    ctx.beginPath();
+    ctx.arc(node.x, node.y, 5, 0, 2 * Math.PI, false);  //the node is represented by a circle
+    ctx.fillStyle = color;  //fill circle color
+    ctx.fill();
+
+    // draw the label
+    ctx.font = `${fontSize}px Sans-Serif`; 
+    ctx.textAlign = 'center';  
+    ctx.textBaseline = 'middle';  
+    ctx.fillStyle = 'black';  
+    ctx.fillText(label, node.x, node.y + 10); 
+  };
+
+
+  //color rules for weak balanced graphs
+  const WeakBalancedNodeColor = useMemo(() => { //depending on the isbalanced result to draw the color of nodes
+    if (!graph_data.isweakbalanced) { //if the graph is not weak balanced
+      return () => 'grey'; // All nodes are grey 
+    }
+  
+    const nodeColorMap = new Map();
+    const firstNodeId = graph_data.originalgraphdata.nodes[0].id;
+    nodeColorMap.set(firstNodeId, 'red'); // First node is red color
+  
+    graph_data.originalgraphdata.links.forEach(link => {
+      const sourceColor = nodeColorMap.get(link.source);
+      if (link.sign === '+') { //if it is positive relation, the node is red color
+        nodeColorMap.set(link.target, sourceColor); 
+      } if(link.sign === '-' && !nodeColorMap.has(link.target)) { //if it is negative relation and the node does not have color, so the node does not belong any current subset, then the node has a random color set r,g,b, each const has a random number, use these three random constants as parameters of rgb color function
+        const r = Math.floor(Math.random() * 128); 
+        const g = Math.floor(Math.random() * 256); 
+        const b = Math.floor(Math.random() * 512); 
+        const newColor = `rgb(${r},${g},${b})`;
+        nodeColorMap.set(link.target, newColor);
+      }
+    });
+    return node => nodeColorMap.get(node.id); //draw the node color
+  }, []);
+
+  //render the nodes of weak balanced graph
+  const RenderWeakBalancedNode = (node, ctx, globalScale) => {
+    const label = `node: ${node.id}`;  
+    const fontSize = 12 / globalScale; 
+    const color = WeakBalancedNodeColor(node); //the color of the node is defined by WeakBalancedNodeColor
+
+    //draw the node
+    ctx.beginPath();
+    ctx.arc(node.x, node.y, 5, 0, 2 * Math.PI, false);  
+    ctx.fillStyle = color;  
+    ctx.fill();
+
+    //draw the label
+    ctx.font = `${fontSize}px Sans-Serif`; 
+    ctx.textAlign = 'center';  
+    ctx.textBaseline = 'middle'; 
+    ctx.fillStyle = 'black';  
+    ctx.fillText(label, node.x, node.y + 10); 
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{ display: 'flex', flexDirection: 'row', height: '100vh' }}>
+      {/*left side of the page*/ }
+      <div style={{ flex: 1, overflow: 'auto' }}>
+        <div>
+          <h2>Node Relation Matrix</h2>
+          <table style={{fontSize: '19px'}}>
+            {/* titles of matrix, include first row and first column */}
+            <thead>
+              <tr>
+                <th>Node</th>
+                {Object.keys(graph).map(node => (
+                  <th key={node}>{node}</th>
+                ))}
+              </tr>
+            </thead>
+
+            {/* traverse the matrixData, for each[i][j], input the value */}
+            <tbody>
+              {matrixData.map((row, i) => (
+                <tr key={i}>
+                  <td>{Object.keys(graph)[i]}</td>
+                  {row.map((cell, j) => (
+                    <td key={j}>
+                      <input
+                        type="text"
+                        value={cell}
+                        onChange={e => handleInputChange(e, i, j)}
+                        style={{ width: '100%' }}                    
+                      />
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          {/* create a button to submit the change of matrix */}
+          <button onClick={handleSubmit}>Submit Changes</button>
+
+          {/* if bool 'isbalanced' is true, then print 'true', vice versa */}
+          <h2>balanced:{graph_data.isbalanced ? 'true' : 'false'}</h2>  
+          <h2>weakly balanced:{graph_data.isweakbalanced ? 'true' : 'false'}</h2> 
+
+          {/* draw the graph */}
+          <ForceGraph2D  
+            graphData={graph_data.originalgraphdata} 
+            linkDirectionalArrowLength={5}
+            linkDirectionalArrowRelPos={1}
+            linkCurvature={0.25}
+            nodeCanvasObject={(node, ctx, globalScale) => RenderBalancedNode(node, ctx, globalScale)}
+            linkLabel={linkLabel}
+          /> 
+        </div>  
+      </div>
+
+      {/*right side of the page*/ }
+      <div style={{ flex: 1, overflow: 'auto' }}>
+        <div>
+          <h2>Node Relation Matrix</h2>
+          <table style={{fontSize: '19px'}}>
+            {/* titles of matrix, include first row and first column */}
+            <thead>
+              <tr>
+                <th>Node</th>
+                {Object.keys(graph).map(node => (
+                  <th key={node}>{node}</th>
+                ))}
+              </tr>
+            </thead>
+
+            {/* traverse the matrixData, for each[i][j], input the value */}
+            <tbody>
+              {matrixData.map((row, i) => (
+                <tr key={i}>
+                  <td>{Object.keys(graph)[i]}</td>
+                  {row.map((cell, j) => (
+                    <td key={j}>
+                      <input
+                        type="text"
+                        value={cell}
+                        onChange={e => handleInputChange(e, i, j)}
+                        style={{ width: '100%' }}                    
+                      />
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          {/* create a button to submit the change of matrix */}
+          <button onClick={handleSubmit}>Submit Changes</button>
+
+          {/* if bool 'isbalanced' is true, then print 'true', vice versa */}
+          <h2>balanced:{graph_data.isbalanced ? 'true' : 'false'}</h2>  
+          <h2>weakly balanced:{graph_data.isweakbalanced ? 'true' : 'false'}</h2>  
+
+          {/* draw the graph */}
+          <ForceGraph2D  
+            graphData={graph_data.originalgraphdata} 
+            linkDirectionalArrowLength={5}
+            linkDirectionalArrowRelPos={1}
+            linkCurvature={0.25}
+            nodeCanvasObject={(node, ctx, globalScale) => RenderWeakBalancedNode(node, ctx, globalScale)}
+            linkLabel={linkLabel}
+          /> 
+        </div>  
+      </div>
+
+    </div>  
   );
-}
+} 
 
 export default App;
---
+++ src/balance.py
@@ -0,0 +1,131 @@
+import json
+import random
+from collections import deque
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+
+def generate_random_graph(num_nodes):
+    # Generate a random signed graph with certain number of nodes
+    graph = {}
+    for i in range(num_nodes): #i represents each node in the graph
+        neighbors = [] #list to store neighbor nodes of this node and their relations
+        for j in range(i+1, num_nodes):
+                sign = random.choice(['+', '-'])  # Randomly choose '+' or '-' sign as relation
+                neighbors.append((chr(65 + j), sign)) #add node j and its sign(relation with i) as a tuple into neighbors list, chr transfers j node into its corresponding captial letter
+        graph[chr(65 + i)] = neighbors #assign neighbors list to this i node
+    return graph
+
+
+num_nodes = 4  #number of nodes in the graph
+graph = generate_random_graph(num_nodes)    #generate a random graph
+
+
+def spanning_tree(graph, start):
+    visited = {v: None for v in graph} #all the nodes in graph is recorded as None
+    visited[start] = '+'   #symbol of first node is recorded as +
+    queue = deque([start]) #the first node is added to a queue
+    tree_links = []
+
+    while queue:
+        node = queue.popleft() #choose one node from the queue
+        for adjacent, sign in graph[node]:   #for all the adjacent nodes for this node
+            if visited[adjacent] is None: #the adjacent node is not visited
+                visited[adjacent] = '+' if visited[node] == sign else '-' #if symbol of this node is same as sign between this node and its adjacent, the symbol of adjacent is +, else is -
+                queue.append(adjacent) #add adjacent node to the queue
+                tree_links.append((node, adjacent, sign)) #tree_link add this tuple
+
+    return visited, tree_links
+
+
+# Check non-tree links
+def is_graph_balanced(graph):
+    start_node = next(iter(graph)) #start node is the first node of the graph 
+    marked_nodes, spanning_tree_links = spanning_tree(graph, start_node) #symbols of all the nodes in the graph, the tuple of first node with its adjacents and signs between them               
+    for u in graph:
+        for v, sign in graph[u]:
+            if (u, v, sign) not in spanning_tree_links and (v, u, sign) not in spanning_tree_links: #the relations of first node is tested by spanning tree, so test all the remaining relations in the graph                                                                                               
+                if marked_nodes[u] == marked_nodes[v] and sign == '-': #if sign is - and symbols of two nodes are the same
+                    return False
+                elif marked_nodes[u] != marked_nodes[v] and sign == '+': #if sign is + and symbols of two nodes are not same
+                    return False
+    return True
+
+
+def is_graph_weakbalanced(graph):
+    for u in graph:
+        for v, uv_sign in graph[u]:
+            for r, vr_sign in graph[v]:
+                if r == u:
+                    continue
+                ur_sign = next((sign for node, sign in graph[u] if node == r), None) #use graph[u] find sign between u and r 
+                if not ur_sign:
+                    continue
+                #if there are two + signs and one - sign, it is not weak balannced
+                if uv_sign == '+' and vr_sign == '+' and ur_sign != '+': 
+                    return False
+                if (uv_sign == '-' and vr_sign == '+' and ur_sign != '-') or \
+                   (uv_sign == '+' and vr_sign == '-' and ur_sign != '-'):
+                    return False
+    return True
+
+
+def convert_to_force_graph_format(graph):  #convert generated graph into force graph format so the system can be drawed
+    nodes = [{'id': node} for node in graph]
+    links = []
+    for node, neighbors in graph.items():
+        for neighbor, sign in neighbors:
+            links.append({
+                'source': node,
+                'target': neighbor,
+                'sign': sign
+            })
+    return {'nodes': nodes, 'links': links}
+
+
+def graph_all_information(force_graph, balanced_result, weakbalanced_result):    #combine force graph and balance result in one function
+    graph_data = {
+    "originalgraphdata": force_graph,
+    "isbalanced": balanced_result,
+    "isweakbalanced": weakbalanced_result
+    }
+    return graph_data
+
+
+def create_graph(graph): #store graph as a json file
+    with open('graph.json', 'w') as file:   
+        json.dump(graph, file, indent=4)
+
+
+def create_force_graph(graph): #store graph_data including force graph and balance property as a json file
+    force_graph = convert_to_force_graph_format(graph)     #convert the random graph to force graph format
+
+    balanced_result = is_graph_balanced(graph)   #calculate the balance property of graph
+
+    weakbalanced_result = is_graph_weakbalanced(graph)
+
+    graph_data = graph_all_information(force_graph, balanced_result, weakbalanced_result) 
+    print(graph_data)
+    with open('graph_data.json', 'w') as file:  
+        json.dump(graph_data, file, indent=4)
+
+
+create_graph(graph)
+create_force_graph(graph)
+print(graph)
+print(convert_to_force_graph_format(graph))
+#print(create_force_graph(graph))
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/update-graph', methods=['POST']) 
+def update_graph():  #receive update graph from App.js
+    data = request.get_json()
+    new_graph = data['graphData']   #get new graph
+    create_graph(new_graph)
+    create_force_graph(new_graph)
+    return jsonify({"message": "Update successful"})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
